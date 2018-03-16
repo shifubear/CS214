e7f7f6b2264923360c89114685ba219c8006a5da
Shion Fukuzawa <shion_fukuzawa@dhcp91-129.calvin.edu>
2018-03-16 15:29:46 -0400

    Finished lab06

---
+++ Syllab/Syllab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Syllab
+//
+//  Created by Shion Fukuzawa on 3/5/18.
+//  Copyright © 2018 Shion Fukuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
