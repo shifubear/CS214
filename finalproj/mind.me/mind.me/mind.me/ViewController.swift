9bab66a575d0ed58213271f52b7cf10af72c55b4
Shion Fukuzawa <shion_fukuzawa@dhcp83-48.calvin.edu>
2018-04-14 15:46:19 -0400

    Finished Lab 7

---
+++ finalproj/mind.me/mind.me/mind.me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mind.me
+//
+//  Created by Shion Fukuzawa on 4/11/18.
+//  Copyright © 2018 Shion Fukuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
